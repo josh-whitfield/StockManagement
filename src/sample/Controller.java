b3aded6e654df4f2a538a5a6105ab55052d71400
Josh <joshwhitfield9919@gmail.com>
2020-03-03 16:25:03 +0000

    Merged from commit and added comment plan

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,16 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 import javax.swing.*;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
@@ -21,7 +28,7 @@ public class Controller {
         try {
             //Store username and encoded password (look into salting as well) as text file somewhere on PC ()
             //On log out, clear text file
-
+//For "Keep me logged in" also save timestamp so it can expire. Date log in saved, date log in expires
             String url = "jdbc:mysql://localhost:3306/stock_management";
             Properties info = new Properties();
             info.put("user","root");
@@ -51,6 +58,18 @@ public class Controller {
     }
 
     public void forgotPassword(ActionEvent actionEvent) {
+        Parent root;
+        try{
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("@../sample/forgotPassword.fxml"),resources);
+            Stage stage = new Stage();
+            stage.setTitle("My New Stage Title");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createAccount(ActionEvent actionEvent) {
