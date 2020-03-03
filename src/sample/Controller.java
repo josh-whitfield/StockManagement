44d0b42d01e41fb0dec59255b8fba071045a0e4e
joshua.whitfield <joshua.whitfield@bhpc2182>
2020-03-03 13:06:55 +0000

    Commit as Requested (Calbash)
    (cherry picked from commit f6245e5d584ee9b826ae3fd446a88ad9cf70e688)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,43 +1,56 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
-import javafx.stage.Stage;
-
+import javafx.scene.control.TextField;
 import javax.swing.*;
-import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class Controller {
     @FXML CheckBox chkStayLoggedIn;
+    @FXML TextField txtUsername;
+    @FXML TextField txtPassword;
 
     public void logIn(ActionEvent actionEvent) {
         //login sql stuff here
-        if (chkStayLoggedIn.isSelected()){
-            JOptionPane.showMessageDialog(null, "Checkbox is selected", "Selected", JOptionPane.INFORMATION_MESSAGE);
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Checkbox is NOT selected", "NOT Selected", JOptionPane.INFORMATION_MESSAGE);
-        }
+        Connection myConn = null;
+        CallableStatement myStmt = null;
+        boolean correctLogin = false;
 
+        try {
+            //Store username and encoded password (look into salting as well) as text file somewhere on PC ()
+            //On log out, clear text file
+
+            String url = "jdbc:mysql://localhost:3306/stock_management";
+            Properties info = new Properties();
+            info.put("user","root");
+            info.put("password","MISB3W1serSQL!");
+
+            myConn = DriverManager.getConnection(url,info);
+            myStmt = myConn.prepareCall("{CALL usp_CheckLogIn(?,?,?)}");
+            myStmt.setString(1,txtUsername.getText());
+            myStmt.setString(2,txtPassword.getText());
+            myStmt.registerOutParameter(3, Types.BIT);
+
+            myStmt.execute();
+            correctLogin = myStmt.getBoolean(3);
+
+            if (correctLogin) {
+                JOptionPane.showMessageDialog(null, "Credentials Valid", "Credentials Valid", JOptionPane.INFORMATION_MESSAGE);
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Incorrect username or password", "Invalid Login", JOptionPane.INFORMATION_MESSAGE);
+                txtPassword.setText("");
+                txtUsername.requestFocus();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void forgotPassword(ActionEvent actionEvent) {
-        Parent root;
-        try{
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("@../sample/forgotPassword.fxml"),resources);
-            Stage stage = new Stage();
-            stage.setTitle("My New Stage Title");
-            stage.setScene(new Scene(root, 450, 450));
-            stage.show();
-            // Hide this current window (if this is what you want)
-            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void createAccount(ActionEvent actionEvent) {
