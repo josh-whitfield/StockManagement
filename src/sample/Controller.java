15e7313687969b1a897b2f2256808aa9ce5d0f1a
Josh <joshwhitfield9919@gmail.com>
2020-03-03 13:00:37 +0000

    Started loading forgot password screen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,15 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
 import javax.swing.*;
+import java.io.IOException;
 
 public class Controller {
     @FXML CheckBox chkStayLoggedIn;
@@ -19,6 +26,18 @@ public class Controller {
     }
 
     public void forgotPassword(ActionEvent actionEvent) {
+        Parent root;
+        try{
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("@../sample/forgotPassword.fxml"),resources);
+            Stage stage = new Stage();
+            stage.setTitle("My New Stage Title");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createAccount(ActionEvent actionEvent) {
