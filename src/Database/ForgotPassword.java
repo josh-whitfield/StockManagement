4f3926291acecd4da628718a80780bdf9f72bfca
joshua.whitfield <joshua.whitfield@BHPC2182>
2020-03-19 11:47:11 +0000

    More Check Security Question items

--- src/Database/ForgotPassword.java
+++ src/Database/ForgotPassword.java
@@ -2,8 +2,29 @@ package database;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Types;
 
 public class ForgotPassword {
+    public static boolean checkSecurityQuestion(int questionIndex, String username, String UserAnswer){
+        try {
+            Connection myConn = database.DB_Connect.connection();
+            assert myConn != null;
+            CallableStatement myStmt = myConn.prepareCall("{CALL usp_CheckSecurityQuestion(?,?,?,?)}");
+            myStmt.setInt(1,questionIndex);
+            myStmt.setString(2, username);
+            myStmt.setString(3, UserAnswer);
+            myStmt.registerOutParameter(4, Types.BIT);
+
+            myStmt.execute();
+            boolean correctAnswer = myStmt.getBoolean(4);
+            myConn.close();
+            return correctAnswer;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public static void UpdatePassword(String username, String password){
         try {
             Connection myConn = database.DB_Connect.connection();
