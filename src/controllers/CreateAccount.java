9f6993dfcb2e951ce587fe782f8e8a8c80938e0f
joshua.whitfield <joshua.whitfield@BHPC2182>
2020-05-01 10:23:07 +0100

    Finished Create ACcount validation

--- src/controllers/CreateAccount.java
+++ src/controllers/CreateAccount.java
@@ -3,13 +3,33 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import references.Hashing;
 
-import javax.swing.*;
-
 public class CreateAccount {
+    @FXML
+    Label lblUsernameInvalid;
+    @FXML
+    Label lblUsernameRequired;
+    @FXML
+    Label lblUsernameTaken;
+    @FXML
+    Label lblInvalidPassword;
+    @FXML
+    Label lblPasswordMismatch;
+    @FXML
+    Label lblPasswordsMissing;
+    @FXML
+    Label lblMissingQuestionOne;
+    @FXML
+    Label lblMissingQuestionTwo;
+    @FXML
+    Label lblMissingQuestionThree;
+    @FXML
+    Label lblAccessLevelRequired;
+
     @FXML
     GridPane createAccountGrid;
     @FXML
@@ -29,67 +49,54 @@ public class CreateAccount {
 
     public void initialize() {
         cbAccessLevel.setItems(FXCollections.observableArrayList("User", "Admin"));
+
+        lblUsernameRequired.setVisible(false);
+        lblUsernameInvalid.setVisible(false);
+        lblUsernameTaken.setVisible(false);
+        lblPasswordsMissing.setVisible(false);
+        lblPasswordMismatch.setVisible(false);
+        lblInvalidPassword.setVisible(false);
+        lblAccessLevelRequired.setVisible(false);
+        lblMissingQuestionOne.setVisible(false);
+        lblMissingQuestionTwo.setVisible(false);
+        lblMissingQuestionThree.setVisible(false);
     }
 
     public void createAccount() {
-        //Validate data
-        String username = txtUsername.getText();
-        String password = txtPassword.getText();
-        String confirmPassword = txtConfirmPassword.getText();
-        String accessLevel = cbAccessLevel.getValue();
-        String answer1 = txtAnswer1.getText();
-        String answer2 = txtAnswer2.getText();
-        String answer3 = txtAnswer3.getText();
+        if (!validateData()) {
+            String username = txtUsername.getText();
+            String password = txtPassword.getText();
+            String accessLevel = cbAccessLevel.getValue();
+            String answer1 = txtAnswer1.getText();
+            String answer2 = txtAnswer2.getText();
+            String answer3 = txtAnswer3.getText();
 
-        StringBuilder missingInfo = new StringBuilder();
+            byte[] passwordSalt = Hashing.createSalt();
+            byte[] answerOneSalt = Hashing.createSalt();
+            byte[] answerTwoSalt = Hashing.createSalt();
+            byte[] answerThreeSalt = Hashing.createSalt();
 
-        if (username.equals("") || username.isEmpty()) missingInfo.append("Please give your username\r\n");
-        if (password.equals("") || password.isEmpty()) missingInfo.append("Please give your password\r\n");
-        if (confirmPassword.equals("") || confirmPassword.isEmpty())
-            missingInfo.append("Please confirm your password\r\n");
-        if (accessLevel.equals("") || accessLevel.isEmpty()) missingInfo.append("Please give an access level\r\n");
-        if (answer1.equals("") || answer1.isEmpty() || answer2.equals("") || answer2.isEmpty() || answer3.equals("") || answer3.isEmpty())
-            missingInfo.append("Please answer all security questions\r\n");
+            database.CreateAccount.saveUserDetails(
+                    username,
+                    Hashing.hashValue(password, passwordSalt),
+                    accessLevel.equals("Admin") ? 1 : 0);
 
-        if (!missingInfo.toString().equals("")) {
-            JOptionPane.showMessageDialog(null, "There is missing information:\r\n" + missingInfo.toString(), "Missing Information", JOptionPane.INFORMATION_MESSAGE);
-            clearPasswordFields();
-        } else {
-            if (!password.equals(confirmPassword)) {
-                JOptionPane.showMessageDialog(null, "Please ensure your passwords match", "Passwords do not match", JOptionPane.INFORMATION_MESSAGE);
-                clearPasswordFields();
-            } else if (database.CreateAccount.checkUsernameTaken(username) > 0) {
-                JOptionPane.showMessageDialog(null, username + " is already taken", "Username taken", JOptionPane.INFORMATION_MESSAGE);
-                txtUsername.setText("");
-                txtUsername.requestFocus();
-            } else {
-                byte[] passwordSalt = Hashing.createSalt();
-                byte[] answerOneSalt = Hashing.createSalt();
-                byte[] answerTwoSalt = Hashing.createSalt();
-                byte[] answerThreeSalt = Hashing.createSalt();
+            database.CreateAccount.saveSecurityQuestions(
+                    username,
+                    Hashing.hashValue(answer1, answerOneSalt),
+                    Hashing.hashValue(answer2, answerTwoSalt),
+                    Hashing.hashValue(answer3, answerThreeSalt));
 
-                database.CreateAccount.saveUserDetails(
-                        username,
-                        Hashing.hashValue(password, passwordSalt),
-                        accessLevel.equals("Admin") ? 1 : 0);
-
-                database.CreateAccount.saveSecurityQuestions(
-                        username,
-                        Hashing.hashValue(answer1, answerOneSalt),
-                        Hashing.hashValue(answer2, answerTwoSalt),
-                        Hashing.hashValue(answer3, answerThreeSalt));
-
-                database.CreateAccount.saveSalt(
-                        username,
-                        passwordSalt,
-                        answerOneSalt,
-                        answerTwoSalt,
-                        answerThreeSalt);
-            }
+            database.CreateAccount.saveSalt(
+                    username,
+                    passwordSalt,
+                    answerOneSalt,
+                    answerTwoSalt,
+                    answerThreeSalt);
         }
     }
 
-    private String validateData() {
+    private boolean validateData() {
         String username = txtUsername.getText();
         String password = txtPassword.getText();
         String confirmPassword = txtConfirmPassword.getText();
@@ -98,37 +105,77 @@ public class CreateAccount {
         String answer2 = txtAnswer2.getText();
         String answer3 = txtAnswer3.getText();
 
-        StringBuilder errorInfo = new StringBuilder();
+        boolean errorsFound = false;
 
-        if (username.equals("") || username.isEmpty())
-            errorInfo.append("Please give your username\r\n");
+        if (username.equals("")) {
+            lblUsernameRequired.setVisible(true);
+            errorsFound = true;
+        } else if (username.length() < 8 || username.length() > 64 || !username.matches("[a-zA-Z0-9-._]+")) {
+            lblUsernameRequired.setVisible(false);
+            lblUsernameInvalid.setVisible(true);
+            errorsFound = true;
+        } else if (database.CreateAccount.checkUsernameTaken(username) > 0) {
+            lblUsernameRequired.setVisible(false);
+            lblUsernameInvalid.setVisible(false);
+            lblUsernameTaken.setVisible(true);
+            errorsFound = true;
+        } else {
+            lblUsernameRequired.setVisible(false);
+            lblUsernameInvalid.setVisible(false);
+            lblUsernameTaken.setVisible(false);
+        }
 
-        if (password.equals("") || password.isEmpty())
-            errorInfo.append("Please give your password\r\n");
-
-        if (confirmPassword.equals("") || confirmPassword.isEmpty())
-            errorInfo.append("Please confirm your password\r\n");
-
-        if (accessLevel.equals("") || accessLevel.isEmpty())
-            errorInfo.append("Please give an access level\r\n");
-
-        if (answer1.equals("") || answer1.isEmpty()
-                || answer2.equals("") || answer2.isEmpty()
-                || answer3.equals("") || answer3.isEmpty())
-            errorInfo.append("Please answer all security questions\r\n");
-
-        if (!password.equals(confirmPassword) && !password.equals("") && !confirmPassword.equals("")) {
-            errorInfo.append("Passwords do not match\r\n");
+        if (password.equals("") || confirmPassword.equals("")) {
+            lblPasswordsMissing.setVisible(true);
             clearPasswordFields();
+            errorsFound = true;
+        } else if (!password.equals(confirmPassword)) {
+            lblPasswordsMissing.setVisible(false);
+            lblPasswordMismatch.setVisible(true);
+            clearPasswordFields();
+            errorsFound = true;
+        } else if (password.length() < 8 || password.length() > 64 ||
+                (!password.matches("[^a-zA-Z0-9 ]") && !password.matches("[a-zA-Z0-9]"))) {
+            lblPasswordsMissing.setVisible(false);
+            lblPasswordMismatch.setVisible(false);
+            lblInvalidPassword.setVisible(true);
+            clearPasswordFields();
+            errorsFound = true;
+        } else {
+            lblPasswordsMissing.setVisible(false);
+            lblPasswordMismatch.setVisible(false);
+            lblInvalidPassword.setVisible(false);
         }
 
-        if (database.CreateAccount.checkUsernameTaken(username) > 0) {
-            errorInfo.append("Username is already taken\r\n");
-            txtUsername.setText("");
-            txtUsername.requestFocus();
+        if (accessLevel == null) {
+            lblAccessLevelRequired.setVisible(true);
+            errorsFound = true;
+        } else {
+            lblAccessLevelRequired.setVisible(false);
         }
 
-        return "";
+        if (answer1.equals("")) {
+            lblMissingQuestionOne.setVisible(true);
+            errorsFound = true;
+        } else {
+            lblMissingQuestionOne.setVisible(false);
+        }
+
+        if (answer2.equals("")) {
+            lblMissingQuestionTwo.setVisible(true);
+            errorsFound = true;
+        } else {
+            lblMissingQuestionTwo.setVisible(false);
+        }
+
+        if (answer3.equals("")) {
+            lblMissingQuestionThree.setVisible(true);
+            errorsFound = true;
+        } else {
+            lblMissingQuestionThree.setVisible(false);
+        }
+
+        return errorsFound;
     }
 
     private void clearPasswordFields() {
