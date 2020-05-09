84ada84ca4e6f437d115e85e3cebc4971112acdd
joshua.whitfield <joshua.whitfield@BHPC2182>
2020-05-09 12:22:00 +0100

    Tableview now working, as is the contents update Need to use inheritance for further details

--- src/controllers/MainPage.java
+++ src/controllers/MainPage.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
@@ -25,7 +26,6 @@ public class MainPage {
 
     private ObservableList<StockData> data;
 
-    //private TableView tvStockTable;
     @FXML
     public void initialize() {
         ObservableList elements = FXCollections.observableArrayList();
@@ -36,38 +36,46 @@ public class MainPage {
         lvContents.setItems(elements);
         lvContents.getSelectionModel().select(0);
 
-        /*TableColumn PKID = new TableColumn("PKID");
-        TableColumn Category = new TableColumn("Category");
-        TableColumn ImageLink = new TableColumn("ImageLink");
+        TableColumn PKID = new TableColumn("PKID");
+        TableColumn Image = new TableColumn("Image");
         TableColumn Name = new TableColumn("Name");
         TableColumn Quantity = new TableColumn("Quantity");
         TableColumn Price = new TableColumn("Price");
-        tvStockTable.getColumns().addAll(PKID,Category,ImageLink,Name,Quantity,Price);*/
+        TableColumn TotalValue = new TableColumn("TotalValue");
+        tvStockTable.getColumns().addAll(PKID, Image, Name, Quantity, Price, TotalValue);
 
         PKID.setCellValueFactory(
                 new PropertyValueFactory<StockData, Integer>("PKID"));
-        Category.setCellValueFactory(
-                new PropertyValueFactory<StockData, String>("Category"));
-        ImageLink.setCellValueFactory(
-                new PropertyValueFactory<StockData, ImageView>("ImageLink"));
+        Image.setCellValueFactory(
+                new PropertyValueFactory<StockData, ImageView>("Image"));
         Name.setCellValueFactory(
                 new PropertyValueFactory<StockData, String>("Name"));
         Quantity.setCellValueFactory(
                 new PropertyValueFactory<StockData, Integer>("Quantity"));
         Price.setCellValueFactory(
                 new PropertyValueFactory<StockData, Double>("Price"));
+        TotalValue.setCellValueFactory(
+                new PropertyValueFactory<StockData, Double>("TotalValue"));
+
+        PKID.setVisible(false);
 
         updateTableView(String.valueOf(lvContents.getSelectionModel().getSelectedItem()));
     }
 
+    @FXML
+    public void handleMouseClick(MouseEvent arg0) {
+        updateTableView(String.valueOf(lvContents.getSelectionModel().getSelectedItem()));
+    }
+
     public void updateTableView(String category) {
+        tvStockTable.getItems().clear();
         data = FXCollections.observableArrayList();
+
         try {
             ResultSet rs = database.MainPage.getTableData(category);
             while (rs.next()) {
                 StockData cm = new StockData();
                 cm.PKID.set(rs.getInt("PKID"));
-                String test = String.format("/resources/images/%s/%s", rs.getString("Category"), rs.getString("ImageLink"));
                 BufferedImage originalImage = ImageIO.read(getClass().getResource(String.format("/resources/images/%s/%s", rs.getString("Category"), rs.getString("ImageLink"))));
                 Image img = SwingFXUtils.toFXImage(originalImage, null);
                 ImageView mv = new ImageView();
@@ -75,50 +83,21 @@ public class MainPage {
                 mv.setFitWidth(70);
                 mv.setFitHeight(80);
                 cm.PKID.set(rs.getInt("PKID"));
-                cm.Category.set(rs.getString("Category"));
-                cm.ImageLink.set(mv);
+                cm.Image.set(mv);
                 cm.Name.set(rs.getString("Name"));
                 cm.Quantity.set(rs.getInt("Quantity"));
                 cm.Price.set(rs.getDouble("Price"));
+                cm.TotalValue.set(rs.getDouble("TotalValue"));
                 data.add(cm);
             }
             tvStockTable.setItems(data);
+
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error on Building Data");
         }
     }
 
-    /*  public void updateTableView(String category) {
-          tableData = FXCollections.observableArrayList();
-          try{
-              ResultSet sqlResults = database.MainPage.getTableData(category);
-              for(int i=0 ; i<sqlResults.getMetaData().getColumnCount(); i++){
-                  final int j = i;
-                  TableColumn col = new TableColumn(sqlResults.getMetaData().getColumnName(i+1));
-                  col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-
-                  tvStockTable.getColumns().addAll(col);
-              }
-
-              while(sqlResults.next()){
-                  //Iterate Row
-                  ObservableList<String> row = FXCollections.observableArrayList();
-                  for(int i=1 ; i<=sqlResults.getMetaData().getColumnCount(); i++){
-                      //Iterate Column
-                      row.add(sqlResults.getString(i));
-                  }
-                  tableData.add(row);
-              }
-
-              //FINALLY ADDED TO TableView
-              tvStockTable.setItems(tableData);
-          }catch(Exception e){
-              e.printStackTrace();
-              System.out.println("Error on Building Data");
-          }
-      }
-  */
     public void searchStock(MouseEvent mouseEvent) {
     }
 }
--- src/references/StockData.java
+++ src/references/StockData.java
@@ -5,32 +5,37 @@ import javafx.beans.property.*;
 public class StockData {
     public SimpleIntegerProperty PKID = new SimpleIntegerProperty();
     public SimpleStringProperty Category = new SimpleStringProperty();
-    public ObjectProperty ImageLink = new SimpleObjectProperty();
+    public ObjectProperty Image = new SimpleObjectProperty();
     public SimpleStringProperty Name = new SimpleStringProperty();
     public SimpleIntegerProperty Quantity = new SimpleIntegerProperty();
     public SimpleDoubleProperty Price = new SimpleDoubleProperty();
+    public SimpleDoubleProperty TotalValue = new SimpleDoubleProperty();
 
     public Integer getPKID() {
         return PKID.get();
     }
 
-    public Object getCategory() {
+    public String getCategory() {
         return Category.get();
     }
 
-    public Object getImageLink() {
-        return ImageLink.get();
+    public Object getImage() {
+        return Image.get();
     }
 
-    public Object getName() {
+    public String getName() {
         return Name.get();
     }
 
-    public Object getQuantity() {
+    public Integer getQuantity() {
         return Quantity.get();
     }
 
-    public Object getPrice() {
+    public Double getPrice() {
         return Price.get();
+    }
+
+    public Double getTotalValue() {
+        return TotalValue.get();
     }
 }
